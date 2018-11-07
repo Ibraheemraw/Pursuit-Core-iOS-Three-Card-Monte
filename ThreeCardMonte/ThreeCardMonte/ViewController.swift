9bc6be135d5573dfa3f165fd7356a9e93213c28b
ibraheemraw <ibraheemrawlinson@pursuit.org>
2018-11-07 15:26:31 -0500

    game completion 95%

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
     //outlet button changing the state
     @IBOutlet weak var cardChoice1: UIButton!
     @IBOutlet weak var cardChoice2: UIButton!
@@ -18,99 +16,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var outComeDescription: UILabel!
     @IBOutlet weak var restart: UIButton!
     @IBOutlet weak var usableCards: UIButton!
-  
-
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    
   restartGame(restart)
-    
-    
-    
   }
-
-    
     //actions
-    
     @IBAction func flip(_ sender: UIButton) {
-        
         var images = [UIImage(named: "kingCard")!,
                       UIImage(named: "threeCard")!, UIImage(named: "threeCard")!]
-            images = images.shuffled()
-        
-    
-        
+        images = images.shuffled()
         switch sender.tag {
         case 0:
            //checking to see if the value is the king card
             if images[0] == UIImage(named: "kingCard")!{
-                cardChoice1.setImage(UIImage.init(named: "kingCard"), for: .normal)
+            cardChoice1.setImage(UIImage.init(named: "kingCard"), for: .normal)
                 outComeDescription.text = "   Cograts you Won! 🎉 "
-                cardChoice2.isEnabled = false
-                cardChoice3.isEnabled = false
-//                restartGame(restart)
-                
+                disableAllCards(usableCards)
+                return
             } else {
-                cardChoice1.setImage(UIImage.init(named: "threeCard"), for: .normal)
+            cardChoice1.setImage(UIImage.init(named: "threeCard"), for: .normal)
                 outComeDescription.text = "  Sorry Try again 🤷🏽‍♂️  "
-//                restartGame(restart)
-                
-                //find out which card choice has the king and flip it
+              disableAllCards(usableCards)
             }
-            
-            //if it is the 3 of diamonds do some work
         case 1:
             if images[1] == UIImage(named: "kingCard")!{
-                cardChoice2.setImage(UIImage.init(named: "kingCard"), for: .normal)
+            cardChoice2.setImage(UIImage.init(named: "kingCard"), for: .normal)
                 outComeDescription.text = "   Cograts you Won! 🎉 "
-                cardChoice1.isEnabled = false
-                cardChoice3.isEnabled = false
-//             restartGame(restart)
-                
+                disableAllCards(usableCards)
             } else {
-                cardChoice2.setImage(UIImage.init(named: "threeCard"), for: .normal)
+            cardChoice2.setImage(UIImage.init(named: "threeCard"), for: .normal)
                 outComeDescription.text = "  Sorry Try again 🤷🏽‍♂️  "
-//                restartGame(restart)
-               
-                //find out which card choice has the king and flip it
+               disableAllCards(usableCards)
             }
-            
-            //if it is the 3 of diamonds do some work
-           
+
         case 2:
             if images[2] == UIImage(named: "kingCard")!{
-                cardChoice3.setImage(UIImage.init(named: "kingCard"), for: .normal)
+            cardChoice3.setImage(UIImage.init(named: "kingCard"), for: .normal)
                 outComeDescription.text = "   Cograts you Won! 🎉 "
-                cardChoice2.isEnabled = false
-                cardChoice1.isEnabled = false
-//                restartGame(restart)
-               
+                disableAllCards(usableCards)
+              
+
             } else {
                 cardChoice3.setImage(UIImage.init(named: "threeCard"), for: .normal)
                 outComeDescription.text = "  Sorry Try again 🤷🏽‍♂️  "
-//               restartGame(restart)
-                //find out which card choice has the king and flip it
+               disableAllCards(usableCards)
             }
-            
-            //if it is the 3 of diamonds do some work
-           
         default:
             print("Error")
         }
     }
-    
     @IBAction func restartGame(_ sender: UIButton) {
         cardChoice1.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
         cardChoice2.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
         cardChoice3.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
         outComeDescription.text = "Pick a Card, Any Card 😏😈"
         enableAllCards(usableCards)
-        
-        
-                
-        
-        
     }
     @IBAction func enableAllCards(_ sender: UIButton){
         cardChoice1.isEnabled = true
@@ -118,5 +79,17 @@ class ViewController: UIViewController {
         cardChoice3.isEnabled = true
     }
     
+    @IBAction func disableAllCards(_ sender: UIButton){
+        cardChoice1.isEnabled = false
+        cardChoice2.isEnabled = false
+        cardChoice3.isEnabled = false
+    }
+    
+    @IBAction func findTheKing(_ sender: UIButton) {
+        
+    }
+    
+    
 }
+
 
